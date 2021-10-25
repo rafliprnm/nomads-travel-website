898f85f02adaed15f98b5ebec2493634fdd31fd8
rafliprnm <rafliprnm@gmail.com>
2021-10-26 06:00:13 +0700

    nomads

---
+++ app/Http/Controllers/Admin/TravelPackageController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\TravelPackageRequest;
+use App\Models\TravelPackage as ModelsTravelPackage;
+use CreateTravelPackagesTable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+
+class TravelPackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = ModelsTravelPackage::all();
+
+        return view('pages.admin.travel-package.index',[
+          'items'=>$items
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.admin.travel-package.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TravelpackageRequest $request)
+    {
+        $data = $request->all();
+        $data['slug']=Str::slug($request->title);
+
+        ModelsTravelPackage::create($data);
+        return redirect()->route('travel-package.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = ModelsTravelPackage::findOrfail($id);
+        return view('pages.admin.travel-package.edit', [
+            'item' => $item
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TravelPackageRequest $request, $id)
+    {
+        $data = $request->all();
+        $data['slug']=Str::slug($request->title);
+
+        $item = ModelsTravelPackage::findOrfail($id);
+        $item -> update($data);
+        return redirect()->route('travel-package.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = ModelsTravelPackage::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('travel-package.index');
+    }
+}
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="section-footer mt-5 mb-4 border-top">
+            <div class="container pt-5 pb-5">
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-12 col-lg-3">
+                                <h5>FEATURES</h5>
+                                <ul class="list-unstyled">
+                                    <li><a href="#"></a>Review</li>
+                                    <li><a href="#"></a>Community</li>
+                                    <li><a href="#"></a>Social Media Kit</li>
+                                    <li><a href="#"></a>Affiliate</li>
+                                </ul>
+                            </div>
+                            <div class="col-12 col-lg-3">
+                                <h5>ACCOUNT</h5>
+                                <ul class="list-unstyled">
+                                    <li><a href="#"></a>Refund</li>
+                                    <li><a href="#"></a>Security</li>
+                                    <li><a href="#"></a>Rewards</li>
+                                </ul>
+                            </div>
+                            <div class="col-12 col-lg-3">
+                                <h5>COMPANY</h5>
+                                <ul class="list-unstyled">
+                                    <li><a href="#"></a>Career</li>
+                                    <li><a href="#"></a>Help Center</li>
+                                    <li><a href="#"></a>Media</li>
+                                </ul>
+                            </div>
+                            <div class="col-12 col-lg-3">
+                                <h5>GET CONNECTED</h5>
+                                <ul class="list-unstyled">
+                                    <li><a href="#"></a>Jakarta Selatan</li>
+                                    <li><a href="#"></a>Indonesia</li>
+                                    <li><a href="#"></a>0821-222-8659</li>
+                                    <li><a href="#"></a>support@nomads.id</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row border-top justify-content-center align-item-center pt-4">
+                    <div class="col-auto text-gray-500 font-weight-light">
+                        2021 Copyright Nomads • All rights reserved • Made in Jakarta
+                    </div>
+                </div>
+            </div>
+        </footer>
